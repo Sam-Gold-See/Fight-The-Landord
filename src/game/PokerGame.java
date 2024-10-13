6bc020dfc7fb3dd5a51adf4d1b4b843f58837981
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-10-14 03:53:43 +0800

    实现了手牌排序

--- src/game/PokerGame.java
+++ src/game/PokerGame.java
@@ -1,55 +1,61 @@
 package game;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class PokerGame {
 
-	static ArrayList<String> list = new ArrayList<>();
+	static ArrayList<Integer> list = new ArrayList<>();
+	static HashMap<Integer, String> hashMap = new HashMap<>();
 
 	static {
 		String[] colors = {"♣", "♦", "♥", "♠"};
 		String[] numbers = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
 
-		for(String color : colors)
-			for(String number : numbers)
-				list.add(color + number);
-		list.add("小王");
-		list.add("大王");
+		int index = 1;
+		for (String number : numbers)
+			for (String color : colors) {
+				list.add(index);
+				hashMap.put(index++, color + number);
+			}
+		list.add(index);
+		hashMap.put(index++, "小王");
+
+		list.add(index);
+		hashMap.put(index++, "大王");
 	}
 
 	public PokerGame() {
 		Collections.shuffle(list);
 
-		ArrayList<String> lord = new ArrayList<>();
-		ArrayList<String> player1 = new ArrayList<>();
-		ArrayList<String> player2 = new ArrayList<>();
-		ArrayList<String> player3 = new ArrayList<>();
+		TreeSet<Integer> lord = new TreeSet<>();
+		TreeSet<Integer> player1 = new TreeSet<>();
+		TreeSet<Integer> player2 = new TreeSet<>();
+		TreeSet<Integer> player3 = new TreeSet<>();
 
-		for(int i=0;i<list.size();i++){
-			String poker = list.get(i);
-			if(i<=2)
+		for (int i = 0; i < list.size(); i++) {
+			int poker = list.get(i);
+			if (i <= 2)
 				lord.add(poker);
-			else if(i%3==0)
+			else if (i % 3 == 0)
 				player1.add(poker);
-			else if(i%3==1)
+			else if (i % 3 == 1)
 				player2.add(poker);
 			else
 				player3.add(poker);
 		}
 
-		lookPoker("底牌",lord);
-		lookPoker("张三",player1);
-		lookPoker("李四",player2);
-		lookPoker("王五",player3);
+		lookPoker("底牌", lord);
+		lookPoker("张三", player1);
+		lookPoker("李四", player2);
+		lookPoker("王五", player3);
+
 	}
 
-	public void lookPoker(String name, ArrayList<String> list) {
-		System.out.print(name + ":");
-		for(String poker:list)
-			System.out.print(poker+" ");
+	public void lookPoker(String name, TreeSet<Integer> pokers) {
+		System.out.print(name + ": ");
+		for (int poker : pokers)
+			System.out.print(hashMap.get(poker) + " ");
 		System.out.println();
 	}
 }
