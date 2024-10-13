7aaa6cd9664aea070166caf546ca6afd839ab0be
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-10-14 04:08:24 +0800

    完善牌与图片素材的设置

--- src/domain/Poker.java
+++ src/domain/Poker.java
@@ -8,7 +8,7 @@ public class Poker {
 	static HashMap<Integer, String> hashMap = new HashMap<>();
 
 	static {
-		String[] colors = {"♣", "♦", "♥", "♠"};
+		String[] colors = {"♠", "♥", "♣", "♦"};
 		String[] numbers = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
 
 		int index = 1;
